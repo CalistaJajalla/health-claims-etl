be5e212bcab8fbeaddc8bdf19fa422210c37c714
Calista Jajalla <calista.jajalla@ftwfoundation.org>
2025-12-04 17:08:10 +0800

    Update Streamlit app, DB connection, .gitignore

--- etl/db.py
+++ etl/db.py
@@ -2,11 +2,10 @@ import os
 from sqlalchemy import create_engine
 
 def get_engine():
-    user = 'user'
-    password = 'password'
-    host = os.getenv('POSTGRES_HOST', 'localhost') 
-    port = '5432'
-    db = 'health_claims'
-    url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
-    engine = create_engine(url)
-    return engine
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("POSTGRES_HOST")
+    port = os.getenv("POSTGRES_PORT")
+    db = os.getenv("POSTGRES_DB")
+    url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+    return create_engine(url, pool_pre_ping=True)
