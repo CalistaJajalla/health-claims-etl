260ebab6e113516c082a423ee611bc2cd6e78190
CaliCodes <calista.jajalla@ftwfoundation.org>
2025-12-05 14:25:19 +0800

    Update db.py

--- etl/db.py
+++ etl/db.py
@@ -1,11 +1,21 @@
 from sqlalchemy import create_engine
+from urllib.parse import urlparse, parse_qsl, urlencode, urlunparse
 
 def get_engine(secrets):
-    # Cloud: use Supabase Pooler
+    # Cloud: use Supabase Pooler URL if available
     if "DATABASE_URL" in secrets:
-        return create_engine(secrets["DATABASE_URL"], pool_pre_ping=True)
+        url = secrets["DATABASE_URL"]
+        # Sanitize URL: remove 'pgbouncer=true' if present
+        parsed_url = urlparse(url)
+        query_params = dict(parse_qsl(parsed_url.query))
+        if 'pgbouncer' in query_params:
+            query_params.pop('pgbouncer')
+            sanitized_query = urlencode(query_params)
+            parsed_url = parsed_url._replace(query=sanitized_query)
+            url = urlunparse(parsed_url)
+        return create_engine(url, pool_pre_ping=True)
 
-    # Local: use normal Postgres connection
+    # Local: use normal Postgres connection from individual secrets
     user = secrets["DB_USER"]
     password = secrets["DB_PASSWORD"]
     host = secrets["POSTGRES_HOST"]
